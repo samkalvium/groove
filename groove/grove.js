ffa8a007a2abb629552a33a9131c364baac41a25
samkalvium <141735202+samkalvium@users.noreply.github.com>
2023-08-11 22:26:27 +0530

    Add files via upload

---
+++ groove/grove.js
@@ -0,0 +1,8 @@
+var varibale_name = document.querySelector(".btn");
+var act_btn = document.querySelector("#buy");
+
+act_btn.onclick = ()=>{
+    
+    varibale_name.innerText = "Thank You for Shopping wth us.";
+
+}
